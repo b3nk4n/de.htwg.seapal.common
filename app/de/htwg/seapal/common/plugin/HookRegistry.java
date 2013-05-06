8cdb3c3fa1ac95d7e9283fd87575d114d5ab1768
Lukas Woehrl <lukas@woehrl.net>
2013-05-06 23:32:43 +0200

    Changed hooks container from list to set

--- app/de/htwg/seapal/common/plugin/HookRegistry.java
+++ app/de/htwg/seapal/common/plugin/HookRegistry.java
@@ -1,35 +1,35 @@
 package de.htwg.seapal.common.plugin;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Map;
 
 import com.google.inject.Singleton;
 
 @Singleton
 public class HookRegistry {
-	Map<String, List<HookHandler<?,?>>> hooks;
+	Map<String, Set<HookHandler<?,?>>> hooks;
 	
 	public HookRegistry(){
-		hooks = new HashMap<String, List<HookHandler<?,?>>>();
+		hooks = new HashMap<String, Set<HookHandler<?,?>>>();
 	}
 	
-	public HookRegistry(HashMap<String, List<HookHandler<?,?>>> hooks){
+	public HookRegistry(HashMap<String, Set<HookHandler<?,?>>> hooks){
 		this.hooks = hooks;
 	}
 	
 	public void registerHook(String hookName, HookHandler<?,?> handler){
 		if(!hooks.containsKey(hookName)){
-			hooks.put(hookName, new LinkedList<HookHandler<?,?>>());
+			hooks.put(hookName, new HashSet<HookHandler<?,?>>());
 		}
 		
 		hooks.get(hookName).add(handler);
 	}
 	
 	@SuppressWarnings("unchecked")
-	public <ReturnType, ArgType> List<HookHandler<ReturnType, ArgType>> getHooks(String hookName, Class<ReturnType> ret, Class<ArgType> arg){
-		List<HookHandler<ReturnType, ArgType>> hooksReturn = new LinkedList<HookHandler<ReturnType,ArgType>>();
+	public <ReturnType, ArgType> Set<HookHandler<ReturnType, ArgType>> getHooks(String hookName, Class<ReturnType> ret, Class<ArgType> arg){
+		Set<HookHandler<ReturnType, ArgType>> hooksReturn = new HashSet<HookHandler<ReturnType,ArgType>>();
 		
 		if(hooks.containsKey(hookName)){
 			for(HookHandler<?,?> hook : hooks.get(hookName)){
